b162e0160b16cc0c9d1ac138f0dc8f9a084464e4
Brian Shisanya <brian.shisanya2000@gmail.com>
2019-03-11 01:15:03 +1100

    Add files via upload

--- LocalFun.js
+++ LocalFun.js
@@ -1,51 +1,35 @@
 const fs = require("fs");
+const csv = require("csvtojson");
 const util = require("util");
 
-function processCvstoJson(filePath) {
-  var csv = fs.readFileSync(filePath, "utf8");
-  var arrGnenerate = 0;
+async function loader(){
+  const promise = new Promise((resolve,reject)=>{
+    csv().fromFile("worldcities.csv").then((jsonObj)=>{
 
-  var result = { Cities0: [], Population0: [], Chunks: [], Citynum: [] };
-  var lines = csv.split("\n");
-
-  for (var i = 1; i < lines.length; i++) {
-    var currentline = lines[i].split(",");
-
-    if (currentline[currentline.length - 2] != '""') {
-      currentline = currentline.map(head => head.replace(/"|\r|\n|\'/gi, "")); // cleaning up the data 
-
-      if (result[`Cities${arrGnenerate}`].length > 99) {
-        arrGnenerate += 1;
-
-        result[`Cities${arrGnenerate}`] = [];
-        result[`Population${arrGnenerate}`] = [];
+      for (let i = 0; i < jsonObj.length; i++) {
+        if (Number(jsonObj[i]["population"].replace(/\s/g,'')) &&
+        isNaN(jsonObj[i]["population"].replace(/\s/g,'')) == false &&
+        Number(jsonObj[i]["population"].replace(/\s/g,'')) != undefined &&
+        jsonObj[i]["city"].replace(/\s/g,'') !=""){
+          jsonObj[i] = {
+            "City":jsonObj[i]["city"],
+            "Pop":Number(jsonObj[i]["population"])
+          };
+        }else{
+          delete jsonObj[i]
+        }
       }
+      // Remove all nulls
+      let formatedJson = JSON.stringify(jsonObj).replace(/null,/g,"");
+      fs.writeFileSync("Simplified.json",formatedJson,"utf-8");
 
-      let num = Number(currentline[9]);
+      resolve(JSON.parse(formatedJson));
+    })
+  })
 
-      if (num != "" && isNaN(num) != true) {
-        result[`Cities${arrGnenerate}`].push(currentline[1]);
-        result[`Population${arrGnenerate}`].push(num);
-      }
-    } else {
-      // Moves on to the next csv line that has the data i want
-      continue;
-    }
-  }
-
-  result["Citynum"] =
-    arrGnenerate * 100 - (100 - result[`Cities${arrGnenerate}`].length);
-  result["Chunks"] = arrGnenerate;
-  // - Write simplified data to a new file.
-  fs.writeFileSync(
-    "Simplified.json",
-    util.inspect(JSON.parse(JSON.stringify(result))),
-    "utf-8"
-  ); // doing this converts from '' to "" which is now valid to parse);
-
-  return result; // Only returns cities that have population
+  return await promise;
 }
 
 module.exports = {
-  processCvstoJson: processCvstoJson
+  loader: loader
 };
--- index.js
+++ index.js
@@ -1,51 +1,40 @@
-const open = require("open");
-const express = require("express");
 const localFun = require("./LocalFun");
+const express = require("express");
 const app = express();
+const open = require("open");
 
-var result = localFun.processCvstoJson("./worldcities.csv");
+(async function(){
+  let cityData =  await localFun.loader();
+  let population = { lowest: [], biggest: [] };
+  let cities = { lowest: [], biggest: [] };
+  let temp = [];
 
-var totalCitiesNum = result["Citynum"],
-  totalSum = null,
-  population = { small: [], large: [] };
-cities = { small: [], large: [] };
-
-// this for loop is used to combined chunks of data array data
-for (let i = 0; i < result["Chunks"] - 1; i++) {
-  totalSum += result[`Population${i}`].reduce((a, b) => a + b);
-  let small = Math.min(...result[`Population${i}`]);
-  let large = Math.max(...result[`Population${i}`]);
-
-  population["small"].push(small);
-  population["large"].push(large);
-  cities["small"].push(
-    result[`Cities${i}`][result[`Population${i}`].indexOf(small)]
-  );
-  cities["large"].push(
-    result[`Cities${i}`][result[`Population${i}`].indexOf(large)]
-  );
-}
-
-var average = Math.round(totalSum / totalCitiesNum);
-var smallestPopulation = Math.min(...population["small"]);
-var largestPopulation = Math.max(...population["large"]);
-var cityBname = cities["large"][population["large"].indexOf(largestPopulation)];
-var citySname =
-  cities["small"][population["small"].indexOf(smallestPopulation)];
-
-// creating a basic graphical interface to display the report  
-app.set("view engine", "ejs");
-app.get("/", function(req, res) {
-  res.render("index", {
-    largest: largestPopulation,
-    smallest: smallestPopulation,
-    cityBname: cityBname,
-    citySname: citySname,
-    average: average
+  cityData.map(pop => temp.push(pop["Pop"]));
+  population["lowest"] = Math.min(...temp);
+  population["biggest"] = Math.max(...temp); 
+  cities["lowest"] = cityData[temp.indexOf(population["lowest"])]["City"];
+  cities["biggest"] = cityData[temp.indexOf(population["biggest"])]["City"];
+  let totalSum = temp.reduce((a, b) => a + b);
+  let average = Math.round(totalSum / cityData.length);
+  
+  // creating a basic graphical interface to display the report  
+  app.set("view engine", "ejs");
+  app.get("/", function(req, res) {
+    res.render("index", {
+      biggest: population["biggest"],
+      lowest: population["lowest"],
+      cityNameLowest: cities["lowest"],
+      cityNameBiggest: cities["biggest"],
+      average: average
+    });
   });
-});
 
-app.listen(process.env.PORT || 8080, function() {
-  console.log("Server in now active on Port 8080 ( http://localhost:8080/ )");
-  open("http://localhost:8080/");
-});
+  app.listen(process.env.PORT || 8080, function() {
+    console.log("Server in now active on Port 8080 ( http://localhost:8080/ )");
+    open("http://localhost:8080/");
+  });
+})();
+
+
+
+
